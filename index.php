60ffdf0ca28f848b10fc7f608154db46f3d48e08
Harselan <haeressel@gmail.com>
2016-12-19 10:22:09 +0100

    A button change
    An attempt to solve an error

--- index.php
+++ index.php
@@ -41,8 +41,8 @@
   <div class="text">Hitta nya spel</div>
 </div>
 
-<a class="prev" onclick="plusSlides(-1)"> < </a>
-<a class="next" onclick="plusSlides(1)">></a>
+<a class="prev" onclick="plusSlides(-1)"> &#10094; </a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
 
 </div>
 <br>
